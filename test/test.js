d81691614f7b9f4b5145553d3b2c9f742565a929
Tom Fenton <tom@ncs.co.nz>
2015-04-21 20:42:04 +1200

    Add test dependencies. jshint globals.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('/lbp', function () {
+  it('should get an LBP', function (done) {
+    expect(1 === 1).to.equal(true);
+    done();
+  });
+});
